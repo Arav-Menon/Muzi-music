14960279ced2f5ed3c14570f7dcf803e9c73d8bf
Arav menon <aravmenon.ak@gmail.com>
2025-06-19 20:25:09 +0530

    fix importes

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
-import { authoptions } from "../../../../../../packages/lib/auth"
+import { authOptions } from "../../../../../../packages/lib/auth"
 // import { authoptions } from "@repo/lib/authoptions"
 
-const handler = NextAuth(authoptions)
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
+
+  console.log(process.env.NEXTAUTH_SECRET)
+
   return (
     <>
-      <div>dsfsd</div>
+     <Link href="/api/auth/signin" className="border-white rounded border-2 text-red-400" >Go to signin page</Link>
+
+
     </>
   );
 }
